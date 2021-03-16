9f24ba4955553b538903f0802a1a836e6429c960
Victor Porton <porton@narod.ru>
2021-03-16 10:23:37 +0200

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sign msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sign from './components/Sign.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sign
   }
 }
 </script>
--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Sign',
   props: {
     msg: String
   }
