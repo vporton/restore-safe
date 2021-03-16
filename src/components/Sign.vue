d8cf65a926c564afba55aacca493579fd595717b
Victor Porton <porton@narod.ru>
2021-03-16 13:10:35 +0200

    creating signatures finished

--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -25,30 +25,53 @@
 
 <script>
 import Web3 from 'web3';
+// import GnosisSafe from "@gnosis.pm/safe-contracts/build/contracts/GnosisSafe.json";
+import IERC20 from "@openzeppelin/contracts/build/contracts/IERC20.json";
 
 export default {
   name: 'Sign',
-  props: {
-    safeAddress: String,
-    token: String,
-    amount: String,
-    recipient: String,
+  data() {
+    return {
+      web3: null,
+      safeAddress: "",
+      token: "",
+      amount: "",
+      recipient: "",
+    };
+  },
+  created() {
+    this.web3 = new Web3(Web3.givenProvider);
   },
   methods: {
-    downloadSignature() {
-      const blob = new Blob([ JSON.stringify("YYY") ], { type: 'application/json' });
+    async downloadSignature() {
+      const sig = await this.calculateSignature();
+      if (sig === undefined) {
+        return;
+      }
+      const blob = new Blob([ sig ], { type: 'application/octet-stream' });
       const url = URL.createObjectURL(blob);
+
+      const accounts = await this.web3.eth.getAccounts();
 
       const a = document.createElement('a');
       a.href = url;
-      a.download = `${web3.eth.defaultAccount}.bin`;
+      a.download = `${accounts[0]}.bin`;
       a.click();
-      document.body.removeChild(a);
+//      document.body.removeChild(a);
     },
-    calculateSignature() {
-      const web3 = new Web3(Web3.givenProvider);
-      const data = ;
-      return "XXX";
+    async calculateSignature() {
+      if (!this.recipient || /^0x0+$/.test(this.recipient)) {
+        alert("No recipient specified!!");
+        return undefined;
+      }
+      const contract = new this.web3.eth.Contract(IERC20.abi, this.safeAddress);
+      const accounts = await this.web3.eth.getAccounts();
+      const tx = contract.methods.transfer(this.recipient, this.amount).encodeABI();
+      // const dataBinary = this.web3.utils.hexToBytes(data);
+      const sig = await this.web3.eth.sign(tx, accounts[0]);
+      console.log(sig)
+
+      return sig;
     },
   },
 }
